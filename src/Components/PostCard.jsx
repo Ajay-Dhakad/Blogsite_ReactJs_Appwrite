9a09b2933ddd412095e173c172dc7a03934ae1e9
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2023-12-26 01:50:04 +0530

    updated...

--- src/Components/PostCard.jsx
+++ src/Components/PostCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import service from '../appwrite/config'
-import {parse} from 'html-react-parser'
+import parse from 'html-react-parser'
 
 function PostCard({$id,title,featuredImage,content}) {
-  return (
+  const truncateContent = (content, numWords) =>
+  content.split(' ').slice(0, numWords).join(' ');    
+  return ( 
     <Link to={`/post/${$id}`}>
 
 <div className='postcard'>
@@ -13,7 +15,7 @@ function PostCard({$id,title,featuredImage,content}) {
     <img  src={service.filePreview(featuredImage)} alt={title} />
 </div>
 <h2>{title}</h2>
-<p>{parse(content)}</p>
+<p className='content-oncard' >{parse(truncateContent(content, 3) +'&nbsp......')}</p>
 
 </div>
     </Link>
