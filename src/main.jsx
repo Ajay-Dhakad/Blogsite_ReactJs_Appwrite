c98173029228a758bf4ccd2816ff794d39f7d06a
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2023-12-28 22:25:33 +0530

    removed unused file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
+// import Home from './pages/Home.jsx'
 import { AuthLayout, Homepage, Login } from './Components/index.js'
 import store from './Store/Store'
 
