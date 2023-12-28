c4b6080abccc918b8229b2b7172a5980d9cf2e25
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2023-12-28 22:40:40 +0530

    updated !

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 // import Home from './pages/Home.jsx'
 import { AuthLayout, Homepage, Login } from './Components/index.js'
-import store from './Store/Store'
+import Store from './Store/Store.js'
 
 import AddPost from "./pages/Addpost";
 import Signup from './pages/Signup'
@@ -78,7 +78,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
     <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
