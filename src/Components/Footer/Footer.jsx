b6e7b8db1127daef1b958bd6e0278ea64ee694ef
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-01 14:46:18 +0530

    Updated Hyperlinks added !

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,10 +8,14 @@ function Footer() {
    <section>  
 
     <div className="socials">
+      <Link to='https://github.com/Ajay-Dhakad/'>
       <img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" alt="Github" className="github" />
+      </Link>
+      <Link to='https://linkedin.com/in/ajay-dhakad-01126324b'>
       <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png" alt="LinkedIn" className="linkedin" />
+      </Link>
     </div>
-   
+  
     <div className="footertext">© 2024 ThoughtCloud</div>
     <hr />
     
