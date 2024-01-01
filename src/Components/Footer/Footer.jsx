1049eb4bf08293a98ce775959f21dff5885a3456
Ajay-Dhakad <ajaydhakad.py@gmail.com>
2024-01-01 13:32:48 +0530

    Updated footer !

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,8 +7,13 @@ function Footer() {
 
    <section>  
 
-    
+    <div className="socials">
+      <img src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png" alt="Github" className="github" />
+      <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png" alt="LinkedIn" className="linkedin" />
+    </div>
+   
     <div className="footertext">© 2024 ThoughtCloud</div>
+    <hr />
     
    </section>
   )
